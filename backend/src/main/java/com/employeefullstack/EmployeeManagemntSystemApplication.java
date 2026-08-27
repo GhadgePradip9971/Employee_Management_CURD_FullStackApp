bcddea2a45ec1f4eacd50e2872994799ba32d61b
Pradip Ghadge <pradipghadge17@gmail.com>
2026-08-27 12:41:43 +0530

    getting started project done "chore: initialize Spring Boot project structure
    - Add Spring Boot application with pom.xml
    - Setup Maven dependencies
    - Configure application properties
    - Prepare project for employee management system"

---
+++ backend/src/main/java/com/employeefullstack/EmployeeManagemntSystemApplication.java
@@ -0,0 +1,13 @@
+package com.employeefullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagemntSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagemntSystemApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/employefullstaksurd/employee_Managemnt_System/EmployeeManagemntSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package employefullstaksurd.employee_Managemnt_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagemntSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
