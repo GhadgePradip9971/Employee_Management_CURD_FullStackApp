13ed2b13d1ed2d821c2a79ab4bdb23da5388d63f
Pradip Ghadge <pradipghadge17@gmail.com>
2026-09-06 17:42:27 +0530

    adding dependecies react-router-dom

--- ems-frontend/src/App.jsx
+++ ems-frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
+import FooterComponent from './component/FooterComponent'
+import HeaderComponent from './component/HeaderComponent'
 import ListEmployeeComponent from './component/ListEmployeeComponent'
 
 function App(){
   return(
     <div>
+    <HeaderComponent/>
    <ListEmployeeComponent/>
+   <FooterComponent/>
     </div>
   )
 }
--- ems-frontend/src/services/EmployeeService.jsx
+++ ems-frontend/src/services/EmployeeService.jsx
@@ -4,4 +4,3 @@ const REST_API_BASE_URL=' http://localhost:8080/api/employees';
 
 export const listEmployees=()=> axios.get(REST_API_BASE_URL);
 
-s
