a2657cc69467c3bab937fd0b3985ef2787fe85f6
Pradip Ghadge <pradipghadge17@gmail.com>
2026-09-18 22:50:56 +0530

    the employee is added succesfully here we call the post method

--- ems-frontend/src/component/EmployeeComponent.jsx
+++ ems-frontend/src/component/EmployeeComponent.jsx
@@ -1,9 +1,14 @@
 import React, { useState } from 'react'
+import { createEmployee } from '../services/EmployeeService'
+import { Navigate, useNavigate } from 'react-router-dom'
 
 const EmployeeComponent = () => {
     const [firstName, setFirstName] = useState('')
     const [lastName, setLastName] = useState('')
     const [email, setEmail] = useState('')
+
+    const navigater=useNavigate();
+
 
     function handleFirstName(e) {
         setFirstName(e.target.value);
@@ -18,6 +23,11 @@ const EmployeeComponent = () => {
         e.preventDefault();
         const employee = { firstName, lastName, email }
         console.log(employee)
+        createEmployee(employee).then((response)=>{
+            console.log(response.data)
+            navigater('/employees')
+
+        })
     }
 
     return (
